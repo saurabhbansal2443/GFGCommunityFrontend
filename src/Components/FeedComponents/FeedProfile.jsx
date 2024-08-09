b4f3cc66c44916da824c198b9a9cf1f063012caf
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2024-08-09 10:41:21 +0530

    feed updated

--- src/Components/FeedComponents/FeedProfile.jsx
+++ src/Components/FeedComponents/FeedProfile.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const FeedProfile = () => {
   return (
-    <div className="bg-slate-200 w-1/5 flex-col items-center justify-center h-[30%] rounded-3xl  border-[0.5px] border-gray-600" >
-      <div className="avatar flex  items-center justify-center pt-[5%]">
-        <div className="w-28 rounded-full">
+    <div className="bg-slate-200 p-2  w-1/6 flex-col items-center justify-center h-[25%] rounded-lg  border-[0.5px] border-gray-600" >
+      <div className="avatar h-1/2 w-full  flex items-center justify-between pt-[5%]">
+        <div className="w-16 rounded-full  ">
           <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+        <div className="flex justify-center items-end  text-sm text-gray-500 flex-col h-2/4 w-3/4">
+          <p className="font-bold"> Saurabh Bansal </p>
+          <p> saurabhbansal2443@gmail.com </p>
         </div>
       </div>
       <div  className="flex  items-center justify-around  mt-[10%]">
